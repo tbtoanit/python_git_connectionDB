9b092567136f924c8506104084e73bad1d5343ff
DELL <son.nguyen377@hcmut.edu.vn>
2021-04-11 10:23:47 +0700

    just for push....

--- da_hinh/demo_da_hinh.py
+++ da_hinh/demo_da_hinh.py
@@ -17,4 +17,5 @@ obj_usa = USA()
 for i in (obj_india,obj_usa):
     i.capital()
     i.languages()
-#ỨNG với 1 tên phương thức được gọi nhưng sẽ có kết quả thực hiện khác nhau.Tùy thuộc vào từng  loại đối tượng
+#ỨNG với 1 tên phương thức được gọi nhưng sẽ có kết quả thực hiện khác nhau.
+#Tùy thuộc vào từng  loại đối tượng.
