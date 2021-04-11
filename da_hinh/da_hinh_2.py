15ad2996cc2f39e5dc8baea86d6fa8dee75d1831
dvanh <vananhvt0241@gmail.com>
2021-04-11 10:28:59 +0700

    change update 3

--- da_hinh/da_hinh_2.py
+++ da_hinh/da_hinh_2.py
@@ -2,19 +2,23 @@ class Str():
     def do_dai(self):
         print("do dai cua chuoi")
 
+
 class Danh_sach():
     def do_dai(self):
         print("so luong phan tu cua danh sach")
 
+
 s = Str()
 d = Danh_sach()
+
 
 def my_len(obj):
     obj.do_dai()
 
+
 my_len(s)
 my_len(d)
-#I want to upload this file again
+# I want to upload this file again
 # I want to push again
 # I want to push again and again
 # push again again
